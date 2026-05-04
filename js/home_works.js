5de745ad8a2c5cc97b8e578bb4f23f785eae92fa
Ayana Myrzabekova <ayana.myrzabekkyzy@gmail.com>
2026-05-04 14:48:24 +0600

    update

--- js/home_works.js
+++ js/home_works.js
@@ -16,15 +16,67 @@ gmailButton.onclick = () => {
 }
 
 //Homework 1 (part 2)
+const parentBlock = document.querySelector ('.parent_block')
 const childBlock = document.querySelector ('.child_block')
 
-let position = 0
+let positionX = 0
+let positionY = 0
+const totalWidth = parentBlock.clientWidth - childBlock.offsetWidth
+const totalHeight = parentBlock.clientHeight - childBlock.offsetHeight
 
 const moveBlock = () => {
-    if (position < 449) {
-        childBlock.style.left =  `${position}px`
-        position++
-       requestAnimationFrame(moveBlock)
+    if (positionX < totalWidth && positionY === 0) {
+        childBlock.style.left = `${positionX}px`;
+        positionX++;
+        requestAnimationFrame(moveBlock);
+    } 
+    else if (positionX >= totalWidth && positionY < totalHeight) {
+        childBlock.style.top = `${positionY}px`;
+        positionY++;
+        requestAnimationFrame(moveBlock);
     }
-}
-moveBlock ()
+    else if (positionX > 0 && positionY >= totalHeight) {
+        childBlock.style.left = `${positionX}px`;
+        positionX--;
+        requestAnimationFrame(moveBlock);
+    } 
+    else if (positionX <= 0 && positionY > 0) {
+        childBlock.style.top = `${positionY}px`;
+        positionY--;
+        requestAnimationFrame(moveBlock);
+    } 
+    else if (positionX === 0 && positionY === 0) {
+        requestAnimationFrame(moveBlock);
+    }
+};
+
+moveBlock();
+
+//Homework 2
+const secondsBlock = document.querySelector('#seconds');
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const resetBtn = document.querySelector('#reset');
+let count = 0;
+let timerId = null;
+let isRunning = false;
+const updateTimer = () => {
+    count++;
+    secondsBlock.innerText = count;
+};
+startBtn.onclick = () => {
+    if (!isRunning) {
+        isRunning = true;
+        timerId = setInterval(updateTimer, 1000);
+    }
+};
+stopBtn.onclick = () => {
+    clearInterval(timerId);
+    isRunning = false;
+};
+resetBtn.onclick = () => {
+    clearInterval(timerId); 
+    isRunning = false;    
+    count = 0;              
+    secondsBlock.innerText = count; 
+};
